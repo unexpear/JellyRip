21ddb5f8215076f98d00d44d3010e9637d0dbc74
Michael <micha@example.com>
2026-03-27 12:41:40 -0400

    build: fix inno compiler and installer paths

--- build_installer.bat
+++ build_installer.bat
@@ -4,6 +4,7 @@ REM Build JellyRip.exe and package JellyRipInstaller.exe with Inno Setup
 set PYTHON_EXE=C:/Users/micha/AppData/Local/Programs/Python/Python313/python.exe
 set ISCC_EXE=C:\Program Files (x86)\Inno Setup 6\ISCC.exe
 if not exist "%ISCC_EXE%" set ISCC_EXE=C:\Program Files\Inno Setup 6\ISCC.exe
+if not exist "%ISCC_EXE%" set ISCC_EXE=%LOCALAPPDATA%\Programs\Inno Setup 6\ISCC.exe
 
 echo Building JellyRip.exe...
 %PYTHON_EXE% -m PyInstaller --onefile --windowed --name JellyRip main.py
